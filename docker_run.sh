0c9815c5797c5df91dd9e39e881b4731526928d9
Master D <Master D@666888.com>
2025-12-27 21:45:50 +0800

    增加了对开放网络的兼容，wol机制更新。

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
 docker run -d \
-  --name my-dashboardv-1130 \
+  --name my-dashboardv-1203 \
   --restart always \
   --pid host \
   --privileged \
@@ -8,7 +8,7 @@ docker run -d \
   -v /home/pi/.vscode-server:/root/.vscode-server \
   -v /home/pi/.vscode:/root/.vscode \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  rpi-dashboard:1130
+  rpi-dashboard:1203
 
 # 打包指令
 docker build -t rpi-dashboard:1130 .
